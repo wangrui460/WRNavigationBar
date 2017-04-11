e922bd374d420a3cb013401838246933cba3cba6
wangrui460 <wangruidev@gmail.com>
2017-04-11 16:47:37 +0800

    demo1基本展示

--- CodeDemo/CodeDemo/FirstViewController.m
+++ CodeDemo/CodeDemo/FirstViewController.m
@@ -8,8 +8,9 @@
 
 #import "FirstViewController.h"
 
-@interface FirstViewController ()
-
+@interface FirstViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIImageView *imgView;
 @end
 
 @implementation FirstViewController
@@ -19,6 +20,68 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor greenColor];
     self.title = @"熊猫美妆";
+    [self.view addSubview:self.tableView];
+    self.tableView.tableHeaderView = self.imgView;
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlack;
+}
+
+#pragma mark - tableview delegate / dataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                                   reuseIdentifier:nil];
+    NSString *str = [NSString stringWithFormat:@"WRNavigationBar %zd",indexPath.row];
+    cell.textLabel.text = str;
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    UIViewController *vc = [UIViewController new];
+    vc.view.backgroundColor = [UIColor darkGrayColor];
+    NSString *str = [NSString stringWithFormat:@"WRNavigationBar %zd",indexPath.row];
+    vc.title = str;
+    [self.navigationController pushViewController:vc animated:YES];
+    vc.navigationController.navigationBar.barStyle = UIBarStyleDefault;
+}
+
+#pragma mark - getter / setter
+- (UITableView *)tableView
+{
+    if (_tableView == nil) {
+        CGRect frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+        _tableView = [[UITableView alloc] initWithFrame:frame
+                                                  style:UITableViewStylePlain];
+        _tableView.contentInset = UIEdgeInsetsMake(-64, 0, 0, 0);
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
+
+- (UIImageView *)imgView
+{
+    if (_imgView == nil) {
+        _imgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"image1"]];
+    }
+    return _imgView;
 }
 
 @end
