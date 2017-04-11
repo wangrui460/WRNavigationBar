6437bb208c4fd55b75d2f0bbb8c63d1f3a1b7781
wangrui460 <wangruidev@gmail.com>
2017-04-11 14:52:39 +0800

    init CodeDemo

---
+++ CodeDemo/CodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeDemo
+//
+//  Created by wangrui on 2017/4/11.
+//  Copyright © 2017年 wangrui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeDemo/CodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeDemo
+//
+//  Created by wangrui on 2017/4/11.
+//  Copyright © 2017年 wangrui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
