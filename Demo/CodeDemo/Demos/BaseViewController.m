3b8dcc22ed8c7064fe0f57d555979577769b945b
wangrui460 <wangruidev@gmail.com>
2017-06-12 18:05:35 +0800

    add to local

--- Demo/CodeDemo/Demos/BaseViewController.m
+++ Demo/CodeDemo/Demos/BaseViewController.m
@@ -5,6 +5,7 @@
 //  Created by wangrui on 2017/5/16.
 //  Copyright © 2017年 wangrui. All rights reserved.
 //
+//  Github地址：https://github.com/wangrui460/WRNavigationBar
 
 #import "BaseViewController.h"
 #import "AppDelegate.h"
--- Demo/CodeDemo/普通/QQMineController.m
+++ Demo/CodeDemo/普通/QQMineController.m
@@ -5,6 +5,7 @@
 //  Created by wangrui on 2017/4/11.
 //  Copyright © 2017年 wangrui. All rights reserved.
 //
+//  Github地址：https://github.com/wangrui460/WRNavigationBar
 
 #import "QQMineController.h"
 #import "WRNavigationBar.h"
