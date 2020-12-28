7f9babe7a5d0130cc821846d9019a28883cfd92b
tanpengsccd <tanpengcd@gmail.com>
2020-12-28 16:32:25 +0800

    1.移除不必要的追踪文件 2.增加通用notch（刘海）判断

--- Demo/CodeDemo/WRNavigationBar/WRNavigationBar.m
+++ Demo/CodeDemo/WRNavigationBar/WRNavigationBar.m
@@ -14,16 +14,25 @@
 @implementation WRNavigationBar
 
 + (BOOL)isIphoneX {
-    struct utsname systemInfo;
-    uname(&systemInfo);
-    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
-    if ([platform isEqualToString:@"i386"] || [platform isEqualToString:@"x86_64"]) {
-        // judgment by height when in simulators
-        return (CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(375, 812)) ||
-                CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(812, 375)));
-    }
-    BOOL isIPhoneX = [platform isEqualToString:@"iPhone10,3"] || [platform isEqualToString:@"iPhone10,6"];
-    return isIPhoneX;
+//    struct utsname systemInfo;
+//    uname(&systemInfo);
+//    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
+//    if ([platform isEqualToString:@"i386"] || [platform isEqualToString:@"x86_64"]) {
+//        // judgment by height when in simulators
+//        return (CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(375, 812)) ||
+//                CGSizeEqualToSize([UIScreen mainScreen].bounds.size, CGSizeMake(812, 375)));
+//    }
+//    BOOL isIPhoneX = [platform isEqualToString:@"iPhone10,3"] || [platform isEqualToString:@"iPhone10,6"];
+//    return isIPhoneX;
+    
+    BOOL iPhoneX = NO;
+       if (@available(iOS 11.0, *)) {
+           UIWindow *mainWindow = [[[UIApplication sharedApplication] delegate] window];
+           if (mainWindow.safeAreaInsets.top > 24.0) {
+               iPhoneX = YES;
+           }
+       }
+    return iPhoneX;
 }
 + (CGFloat)navBarBottom {
     return [self isIphoneX] ? 88 : 64;
